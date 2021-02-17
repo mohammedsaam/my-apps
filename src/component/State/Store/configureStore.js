f498a96fefaa01bef9648b2d12968e944dbbed09
Mohammedsam81 <76901329+Mohammedsam81@users.noreply.github.com>
2021-02-17 16:23:12 +0530

    redux

---
+++ src/component/State/Store/configureStore.js
@@ -0,0 +1,14 @@
+import {createStore,combineReducers}from 'redux';
+import {groupReducer} from '../Reducer/groupReducer';
+
+
+export const configureStore =()=>{
+    const store = createStore(
+combineReducers ({
+    groupReducer,
+}),
+
+window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+    return store;
+};
--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {Provider} from 'react-redux';
+import {configureStore} from './component/State/Store/configureStore';
+import {BrowserRouter} from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <Provider store={configureStore()}>
     <App />
+    </Provider>
+    </BrowserRouter>
+   
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
